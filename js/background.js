90915498238591814a6495223bdc9c555dfc2744
huotaol <huotao1992@foxmail.com>
2022-11-22 14:58:46 +0800

    feature: 查询明细，导出所有图片

--- js/background.js
+++ js/background.js
@@ -1,23 +1,23 @@
 //点击下载数据
 function fetchVehicleData() {
-    const arr = [{name:'在售',body:ON_SALE},
-        {name:'在售已上架',body:yishangjia},
-        {name:'寄售代销',body:car_tt_sale},
-        {name:'在售未上架',body:car_zaishouweishangjia},
-        {name:'已售',body:car_sold},
-        {name:'已预定',body:car_booked},
-        {name:'退库',body:car_retreat_from_storage},
-        {name:'交强险30天内',body:car_jiaoqiangxiandaoqi_30},
-        {name:'商业险到期30天内',body:car_shangyexiandaoqi_30},
-        {name:'年检到期30天内',body:car_car_nianjiandaoqi_30},
-        {name:'联盟代销',body:car_LMDX},
+    const arr = [{name: '在售', body: ON_SALE},
+        {name: '在售已上架', body: yishangjia},
+        {name: '寄售代销', body: car_tt_sale},
+        {name: '在售未上架', body: car_zaishouweishangjia},
+        {name: '已售', body: car_sold},
+        {name: '已预定', body: car_booked},
+        {name: '退库', body: car_retreat_from_storage},
+        {name: '交强险30天内', body: car_jiaoqiangxiandaoqi_30},
+        {name: '商业险到期30天内', body: car_shangyexiandaoqi_30},
+        {name: '年检到期30天内', body: car_car_nianjiandaoqi_30},
+        {name: '联盟代销', body: car_LMDX},
     ];
     for (let requestBody of arr) {
         fetchVehicleDataByParam(requestBody);
     }
 }
 
-function fetchVehicleDataByParam(params){
+function fetchVehicleDataByParam(params) {
     const {name, body} = params;
     body.pageSize = 200;
     fetch("http://crazyracing-kartrider.souche.com/web/v3/carViewQuery/queryRecordPageInfoForPc.json",
@@ -30,7 +30,7 @@ function fetchVehicleDataByParam(params){
         .then(json => {
             console.info("数据拉取成功", json)
             const records = json.data.common.records;
-            exportVehicleData('车源数据-'+name,records)
+            exportVehicleData('车源数据-' + name, records)
         })
         .catch(function (err) {
             console.log('Fetch错误:' + err);
@@ -38,17 +38,17 @@ function fetchVehicleDataByParam(params){
 }
 
 
-
-function exportVehicleData(fileName,records) {
+function exportVehicleData(fileName, records) {
     const header = ['车辆来源', '车辆状态', '库存状态', '微店上架', '品牌', '车系', '车型', '首次上牌', '表显里程', '门店', '库龄', '排放标准', '评估师', '排量', 'VIN码', '车辆编号', '出厂日期',
         '网络标价', '采购类型', '采购价', '采购日期', '展厅标价', '销售底价', '批发价', '新车指导价', '库存描述', '库存描述', "图片"];
     const list = records.map(v => {
         const fieldObj = {};
-
+        const {recordId} = v;
         v.carRecord.fields.map(f => {
             const {code, value, displayValue} = f;
             fieldObj[code] = displayValue || value
         });
+
         const arr = [];
         arr.push(_.get(fieldObj, 'car_field_source_car'))
         arr.push(_.get(fieldObj, 'car_field_operation_phase'))
@@ -82,15 +82,48 @@ function exportVehicleData(fileName,records) {
         arr.push(_.get(fieldObj, 'car_field_manager_price'))
         const pic = _.get(v, 'carRecord.carPicture');
         arr.push(pic);
-        const dir = vin + "_" + brand + series + "-" + getTimeStr();
+        const dir = vin + "_" + brand + series + "-" + recordId;
         download(pic, dir);
+        fetchVehicleDetail(dir, recordId);
         return arr;
     });
     exportFile(fileName, header, list);
 
 }
 
-function getTimeStr(){
+/**
+ * 拉取车辆详细信息
+ * @param recordId
+ * @param dir
+ */
+function fetchVehicleDetail(dir, recordId) {
+    const url = 'http://crazyracing-kartrider.souche.com/web/carEditQuery/queryCarMediaValues.json?carId=' + recordId;
+    fetch(url)
+        .then(response => response.json())
+        .then(resp => {
+            const {code, data, success} = resp;
+            if (success && code == '200') {
+                const {
+                    picture_type_Zi6ZJWxJ7l,
+                    picture_type_UYeyTWcMNN,
+                    picture_type_wbQvzWxEjp
+                } = data.picturesFieldValues;
+
+                let arr = [].concat(picture_type_Zi6ZJWxJ7l);
+                arr = arr.concat(picture_type_UYeyTWcMNN);
+                arr = arr.concat(picture_type_wbQvzWxEjp);
+                for (let temp of arr) {
+                    const {name, content} = temp;
+                    if (name === '照片链接') {
+                        download(dir, content);
+                    }
+                }
+            }
+        });
+}
+
+
+function getTimeStr() {
     const date = new Date();
     return [date.getFullYear(), date.getMonth(), date.getDay()].join("");
 }
@@ -117,7 +150,7 @@ function fetchAccountData() {
 
 /*导出客户数据*/
 function exportAccount(records) {
-    const header = [ '姓名', "电话", "区域", "重点客户", "预算", "预计买车时间", "意向车系", "下次跟进时间", "最近跟进内容",
+    const header = ['姓名', "电话", "区域", "重点客户", "预算", "预计买车时间", "意向车系", "下次跟进时间", "最近跟进内容",
         "销售", "客户来源"];
     const list = records.map(v => {
         const arr = [];
@@ -191,15 +224,17 @@ function exportOrder(records) {
 }
 
 
-function download(url,dir) {
-    const f=  url.replace(/http.*\//,'')
-    console.log('下载文件', url);
-    chrome.downloads.download({
-        url: url,
-        filename: dir+'/'+f,
-        saveAs: !1,
-        conflictAction: "overwrite"
-    });
+function download(url, dir) {
+    if (url && url.startsWith("http")) {
+        const f = url.replace(/http.*\//, '');
+        chrome.downloads.download({
+            url: url,
+            filename: dir + '/' + f,
+            saveAs: !1,
+            conflictAction: "overwrite"
+        });
+    }
+
 }
 
 
